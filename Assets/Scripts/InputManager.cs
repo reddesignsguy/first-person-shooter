6f521697308cfb51af08f1860dd3fa67fad5108f
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-09-18 22:27:47 -0700

    a hopefully scalable system for commands based on what item is held and what item is being looked at

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -47,4 +49,181 @@ public class InputManager : MonoBehaviour
     {
         onFoot.Disable();
     }
+}
+
+
+public class CommandsManager
+{
+    // DI Held
+    // DI LookingAt
+
+    List<ICommand> commands;
+    ItemContext _context;
+    public CommandsManager()
+    {
+        _context = new ItemContext();
+        CommandValidator validator = new CommandValidator(_context);
+
+        _context._itemHeld = new Scooper();
+        _context._itemLookingAt = new FoodSource();
+
+        commands = new List<ICommand>();
+
+        ScoopCommand scoop = new ScoopCommand(_context);
+        commands.Add(scoop);
+
+        HoldCommand hold = new HoldCommand(_context);
+        commands.Add(hold);
+
+    }
+
+    public void GetCommands()
+    {
+
+        List<ICommand> validCommands = new List<ICommand>();
+
+        foreach (ICommand command in commands)
+        {
+            if (CommandValidator.CommandIsValid(command))
+            {
+                validCommands.Add(command);
+            }
+            else
+            {
+                Console.WriteLine("FAILED TO ADD COMMAND: " + command);
+            }
+            Console.WriteLine("-------------------");
+        }
+        foreach (ICommand command in validCommands)
+        {
+            command.Execute();
+        }
+    }
+}
+
+public class CommandValidator
+{
+
+    public static ItemContext _context;
+
+    public CommandValidator(ItemContext context)
+    {
+        _context = context;
+    }
+
+    public static bool CommandIsValid(ICommand command)
+    {
+
+        Type itemInHand = _context._itemHeld.GetType();
+        Type itemLookAt = _context._itemLookingAt.GetType();
+        Console.WriteLine(itemInHand);
+        Console.WriteLine(itemLookAt);
+
+        Type requiredHand = command._requirementsToExecute.TItemInHand;
+        Type requiredLookAt = command._requirementsToExecute.TItemLookingAt;
+        Console.WriteLine(requiredHand);
+        Console.WriteLine(requiredLookAt);
+
+        bool canUse = requiredHand.IsAssignableFrom(itemInHand) && requiredLookAt.IsAssignableFrom(itemLookAt);
+        if (canUse)
+        {
+            return true;
+        }
+
+        Console.WriteLine("ERROR: Cannot use");
+        return false;
+    }
+}
+
+public class CommandRequirements
+{
+
+    public Type TItemInHand;
+    public Type TItemLookingAt;
+
+    public CommandRequirements(Type TItemInHand, Type TItemLookingAt)
+    {
+        this.TItemInHand = TItemInHand;
+        this.TItemLookingAt = TItemLookingAt;
+    }
+
+}
+
+public abstract class ICommand
+{
+    // Items required for this command
+    public CommandRequirements _requirementsToExecute;
+    private ItemContext _context; // Used explicitly for Execute behavior
+
+    public ICommand(Type TItemInHand, Type TItemLookingAt, ItemContext _context)
+    {
+        this._context = _context;
+
+        _requirementsToExecute = new CommandRequirements(TItemInHand, TItemLookingAt);
+    }
+
+    public void Execute()
+    {
+
+        // Extra layer of defense
+        Console.WriteLine("Executing!");
+        if (!CommandValidator.CommandIsValid(this))
+        {
+            return;
+        }
+        ExecutionBehaviorOverrideMe();
+    }
+
+    protected abstract void ExecutionBehaviorOverrideMe();
+    //public virtual void Execute(); // In base: Do a verification check again with the item in hand  and item looking at
+}
+
+public class ScoopCommand : ICommand
+{
+    public ScoopCommand(ItemContext _context) : base(typeof(Scooper), typeof(FoodSource), _context)
+    {
+    }
+
+    protected override void ExecutionBehaviorOverrideMe()
+    {
+        Console.WriteLine("scopoping!");
+    }
+}
+
+
+public class HoldCommand : ICommand
+{
+    public HoldCommand(ItemContext _context) : base(typeof(EmptyItem), typeof(Item), _context)
+    {
+    }
+
+    protected override void ExecutionBehaviorOverrideMe()
+    {
+        Console.WriteLine("holding!");
+    }
+}
+
+public class Item
+{
+
+}
+
+public class EmptyItem
+{
+
+}
+public class Scooper : Item
+{
+    public float capacity;
+}
+
+public class FoodSource : Item
+{
+
+}
+
+public class ItemContext
+{
+    public Item _itemHeld { get; set; }
+    public Item _itemLookingAt { get; set; }
 }
