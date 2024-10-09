4ddcebc131327a35276f3bc398410d243330a567
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-10-09 15:08:15 -0700

    recipe scriptable object

--- Assets/MixerToggle.cs
+++ Assets/MixerToggle.cs
@@ -2,10 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MixerToggle : Toggleable
 {
+
+    private GameObject result;
+
+    // TODO refactor
+    public Recipe requirement;
+
     public override void Toggle()
     {
-        print("toggling");
+        if (transform.TryGetComponent(out IngredientReceiver receiver))
+        {
+            foreach(var pair in receiver.ingredientQuantities)
+            {
+                Ingredient ingredient = pair.Key;
+                float amount = pair.Value;
+
+                
+            }
+        }
+
+        // Get Ingredient Receiver component
+        // Check ingredients
+        // If ingredients are correct, then output GOOD dough
+        // else: output BAD dough
     }
 }
---
+++ Assets/Recipe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Recipe
+//{
+//    Dictionary<Ingredient, float> ingredients;
+
+//    public Recipe()
+//    {
+//        ingredients = new Dictionary<Ingredient, float>();
+//    }
+
+//    public Recipe Add(Ingredient i, float amt)
+//    {
+//        ingredients[i] = amt;
+//        return this;
+//    }
+//}
+
+[CreateAssetMenu(fileName = "NewRecipe", menuName = "Recipe")]
+public class Recipe : ScriptableObject
+{
+    [Header("Ingredients")]
+    public List<Ingredient> ingredientList;
+    public List<float> amounts;
+
+    public Dictionary<Ingredient, float> ToDictionary()
+    {
+        Dictionary<Ingredient, float> ingredientDictionary = new Dictionary<Ingredient, float>();
+
+        // Check if both lists have the same count
+        if (ingredientList.Count != amounts.Count)
+        {
+            Debug.LogError("Ingredient list and amounts list must have the same count.");
+            return ingredientDictionary; // Return an empty dictionary in case of error
+        }
+
+        // Populate the dictionary
+        for (int i = 0; i < ingredientList.Count; i++)
+        {
+            ingredientDictionary[ingredientList[i]] = amounts[i];
+        }
+
+        return ingredientDictionary;
+    }
+}
